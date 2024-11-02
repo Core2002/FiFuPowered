cae929df5979f6c2dc50d76379f7cf71c6a41511
NekokeCore <core2002@163.com>
2024-11-03 04:45:51 +0800

    👷 Chore: Enhance security and structure of file upload process.
    - Add setAllowedTimePeriodDiscrepancy(3) to DefaultCodeVerifier for better security
    - Refactor code to improve readability and maintainability
    - Split file upload and code validation logic into separate functions
    - Add detailed comments to explain functionality

--- fifu-server-backup/src/main/kotlin/fun/fifu/serverbackup/DataServer.kt
+++ fifu-server-backup/src/main/kotlin/fun/fifu/serverbackup/DataServer.kt
@@ -28,7 +28,9 @@ object DataServer {
     private val vertx = Vertx.vertx()
     private val timeProvider = SystemTimeProvider()
     private val codeGenerator = DefaultCodeGenerator(HashingAlgorithm.SHA512)
-    private val verifier = DefaultCodeVerifier(codeGenerator, timeProvider)
+    private val verifier = DefaultCodeVerifier(codeGenerator, timeProvider).apply {
+        setAllowedTimePeriodDiscrepancy(3)
+    }
 
     /**
      * Starts an HTTP server and sets the size limit for uploaded files.
@@ -50,44 +52,9 @@ object DataServer {
         // Enable body handling with file uploads going to a temporary directory
         router.route().handler(bodyHandler)
 
-        router.post("/upload").handler { routingContext ->
-            // Check if the user has the required permissions to access this resource.
-            val codeForClient = routingContext.request().getHeader("Authorization")
-            try {
-                if (!verifier.isValidCode(configPojo.sendRemoteServerSecret, codeForClient)) {
-                    handleFileDeletion(routingContext)
-                    routingContext.response().setStatusCode(401).end("Unauthorized")
-                    return@handler
-                }
-            } catch (e: Exception) {
-                handleFileDeletion(routingContext)
-                routingContext.response().setStatusCode(402).end("Unauthorized, Has exception in ValidCode")
-                return@handler
-            }
-
-            val fileUploads = routingContext.fileUploads()
-            if (fileUploads.isEmpty()) {
-                routingContext.response().setStatusCode(400).end("No file uploaded")
-                return@handler
-            }
-
-            for (fileUpload in fileUploads) {
-                val uploadedFileName = fileUpload.uploadedFileName()
-                val fileName = fileUpload.fileName()
-                val fileSize = fileUpload.size()
-
-                println("Received file: $fileName (size: $fileSize bytes)")
-
-                // Move the file to a permanent location if needed
-                vertx.fileSystem().move(uploadedFileName, Paths.get("uploads", fileName).toString()) {
-                    if (it.succeeded()) {
-                        routingContext.response().end("File uploaded successfully")
-                    } else {
-                        routingContext.response().setStatusCode(200).end("Ok, but failed to move uploaded file")
-                    }
-                }
-            }
-        }
+        router.post("/upload")
+            .handler(this::validateCode)
+            .handler(this::uploadFile)
 
         server.requestHandler(router).listen(port) { http ->
             if (http.succeeded()) {
@@ -107,5 +74,70 @@ object DataServer {
             println("An unverified attacker has been intercepted. The upload of the file <$uploadedFileName> has been canceled. Their IP address is <$clientIp>.")
             vertx.fileSystem().delete(uploadedFileName)
         }
+    }
+
+    /**
+     * Handles file upload requests.
+     *
+     * This function first checks if there are any file uploads in the RoutingContext.
+     * If not, it ends the response with a 400 status code and an error message.
+     * If files are uploaded, it iterates through each file, logs the file name and size,
+     * and moves the uploaded file to a permanent storage location.
+     * If the file movement is successful, it ends the response with a success message.
+     * If the file movement fails, it ends the response with a 200 status code and a failure message.
+     *
+     * @param routingContext The context of the routing request, containing the uploaded file information.
+     */
+    private fun uploadFile(routingContext: RoutingContext) {
+        val fileUploads = routingContext.fileUploads()
+        if (fileUploads.isEmpty()) {
+            routingContext.response().setStatusCode(400).end("No file uploaded")
+            return
+        }
+
+        for (fileUpload in fileUploads) {
+            val uploadedFileName = fileUpload.uploadedFileName()
+            val fileName = fileUpload.fileName()
+            val fileSize = fileUpload.size()
+
+            println("Received file: $fileName (size: $fileSize bytes)")
+
+            // Move the file to a permanent location if needed
+            vertx.fileSystem().move(uploadedFileName, Paths.get("uploads", fileName).toString()) {
+                if (it.succeeded()) {
+                    routingContext.response().end("File uploaded successfully")
+                } else {
+                    routingContext.response().setStatusCode(200).end("Ok, but failed to move uploaded file")
+                }
+            }
+        }
+    }
+
+
+    /**
+     * Validates the client's access authorization.
+     *
+     * This function checks if the client has provided a valid authorization code. If not, it returns an unauthorized error.
+     * If the authorization is valid, it allows the request to proceed to the next handler.
+     *
+     * @param routingContext The Vert.x routing context containing the request and response objects, among others.
+     */
+    private fun validateCode(routingContext: RoutingContext) {
+        // Check if the user has the required permissions to access this resource.
+        val codeForClient = routingContext.request().getHeader("Authorization")
+        try {
+            if (!verifier.isValidCode(configPojo.sendRemoteServerSecret, codeForClient)) {
+                handleFileDeletion(routingContext)
+                routingContext.response().setStatusCode(401).end("Unauthorized")
+                return
+            }
+        } catch (e: Exception) {
+            handleFileDeletion(routingContext)
+            routingContext.response().setStatusCode(402).end("Unauthorized, Has exception in ValidCode")
+            return
+        }
+
+        // If the code is valid, proceed to the next handler in the chain.
+        routingContext.next()
     }
 }
